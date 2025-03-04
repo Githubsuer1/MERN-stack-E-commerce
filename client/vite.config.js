7ac02adf4326a914379c16e88fc1872cced01eb9
Githubsuer1 <mishrasiddhant560@gmail.com>
2025-03-04 14:25:25 +0530

    vite.config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,9 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server:{
-    proxy:{
-      '/api':"http://localhost:8000"
-    }
+    
   },
   
 })
+// proxy:{
+    //   '/api':"http://localhost:8000"
+    // }
