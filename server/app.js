f02d2e857ddedb4c43e686312017dac0e2066b51
Githubsuer1 <mishrasiddhant560@gmail.com>
2025-03-05 12:53:04 +0530

    app.js updated

--- server/app.js
+++ server/app.js
@@ -16,12 +16,6 @@ app.use(cors({
     allowedHeaders: 'Content-Type, Authorization'
   }));
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://shopistyle.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
   
 app.use(cookieParser());
 
